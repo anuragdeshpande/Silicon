76cb34df219287871f77c60e3f64bec6591325e6
dhysell <dhysell@idfbins.com>
2019-01-08 16:58:18 -0700

    Updating DB connection info

--- src/main/java/framework/database/ConnectionManager.java
+++ src/main/java/framework/database/ConnectionManager.java
@@ -3,21 +3,19 @@ package framework.database;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.commons.dbutils.QueryRunner;
 
-import javax.sql.DataSource;
-
 public class ConnectionManager {
-    private static BasicDataSource ds = new BasicDataSource();
+    private static BasicDataSource testResultsDS = new BasicDataSource();
 
     static {
-        ds.setUrl("jdbc:sqlserver://fbms2048:1433;DatabaseName=QAWIZPROGlobalDataRepository");
-        ds.setUsername("SeleniumUser");
-        ds.setPassword("seleniumuser");
-        ds.setMinIdle(5);
-        ds.setMaxIdle(10);
-        ds.setMaxOpenPreparedStatements(100);
+        testResultsDS.setUrl("jdbc:sqlserver://FBMSGWSQL-RR01:1433;DatabaseName=Regression");
+        testResultsDS.setUsername("sa");
+        testResultsDS.setPassword("password");
+        testResultsDS.setMinIdle(5);
+        testResultsDS.setMaxIdle(10);
+        testResultsDS.setMaxOpenPreparedStatements(100);
     }
 
     public static QueryRunner getRegressionDB() {
-        return new QueryRunner(ds);
+        return new QueryRunner(testResultsDS);
     }
 }
