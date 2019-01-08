fe5996bbd87a80abc27f7dba2a629b8f35000067
dhysell <dhysell@idfbins.com>
2019-01-08 16:53:35 -0700

    Adding Listener implementation, recording tests to db

--- src/main/java/framework/listener/Listener.java
+++ src/main/java/framework/listener/Listener.java
@@ -1,5 +1,7 @@
 package framework.listener;
 
+import framework.database.ConnectionManager;
+import framework.webdriver.DriverManager;
 import org.testng.*;
 
 public class Listener implements ISuiteListener, ITestListener {
@@ -21,30 +23,17 @@ public class Listener implements ISuiteListener, ITestListener {
 
     @Override
     public void onTestSuccess(ITestResult iTestResult) {
-        System.out.println(System.getProperty("jenkinsBuildNumber"));
-        System.out.println(iTestResult.getName());
-        System.out.println(iTestResult.getMethod().getMethodName());
-        System.out.println(iTestResult.getStartMillis());
-        System.out.println(iTestResult.getEndMillis());
-        System.out.println(iTestResult.getStatus());
-        System.out.println(iTestResult.getTestContext().getSuite().getName());
+        DriverManager.recordTestResult(iTestResult, "Success");
     }
 
     @Override
     public void onTestFailure(ITestResult iTestResult) {
-        System.out.println(System.getProperty("jenkinsBuildNumber"));
-        System.out.println(iTestResult.getName());
-        System.out.println(iTestResult.getMethod().getMethodName());
-        System.out.println(iTestResult.getStartMillis());
-        System.out.println(iTestResult.getEndMillis());
-        System.out.println(iTestResult.getStatus());
-        System.out.println(iTestResult.getThrowable().getLocalizedMessage());
-        System.out.println(iTestResult.getTestContext().getSuite().getName());
+        DriverManager.recordTestResult(iTestResult, "Failure");
     }
 
     @Override
     public void onTestSkipped(ITestResult iTestResult) {
-
+        DriverManager.recordTestResult(iTestResult, "Skipped");
     }
 
     @Override
