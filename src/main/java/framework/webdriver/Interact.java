7719e0064eea27fe3c3459a335e6666b312c3b48
Anurag Deshpande <adeshpande@idfbins.com>
2019-03-07 12:53:31 -0700

    Adding support for textarea

--- src/main/java/framework/webdriver/Interact.java
+++ src/main/java/framework/webdriver/Interact.java
@@ -56,6 +56,10 @@ public class Interact {
         return new UITextbox(identifier);
     }
 
+    public UITextbox withTextArea(Identifier identifier) {
+        return new UITextbox(identifier);
+    }
+
     public UISelectBox withSelectBox(Identifier identifier) {
         throw new NotImplementedException();
     }
