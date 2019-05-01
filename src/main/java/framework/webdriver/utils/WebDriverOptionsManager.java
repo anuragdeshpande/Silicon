2de6621ea285aa13182c525d22af5556bd0c3b1a
Anurag Deshpande <adeshpande@idfbins.com>
2019-05-01 10:51:23 -0600

    initial commit for thread management without TestNG

---
+++ src/main/java/framework/webdriver/utils/WebDriverOptionsManager.java
@@ -0,0 +1,21 @@
+package framework.webdriver.utils;
+
+import org.openqa.selenium.UnexpectedAlertBehaviour;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class WebDriverOptionsManager {
+
+    public ChromeOptions getChromeOptions(){
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--start-maximized");
+        chromeOptions.addArguments("--no-sandbox"); //https://stackoverflow.com/a/50725918/1689770
+        chromeOptions.addArguments("--disable-infobars"); //https://stackoverflow.com/a/43840128/1689770
+        chromeOptions.addArguments("--disable-dev-shm-usage"); //https://stackoverflow.com/a/50725918/1689770
+        chromeOptions.addArguments("--disable-browser-side-navigation"); //https://stackoverflow.com/a/49123152/1689770
+        chromeOptions.addArguments("--disable-gpu");
+//        chromeOptions.setUnhandledPromptBehaviour(UnexpectedAlertBehaviour.ACCEPT);
+
+        return chromeOptions;
+    }
+
+}
