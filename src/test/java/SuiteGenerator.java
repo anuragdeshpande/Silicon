35c93a20596ecaa60464ef324ba56d4339df1d84
Anurag Deshpande <adeshpande@idfbins.com>
2019-06-19 11:59:54 -0600

    fixing suite generator

--- src/test/java/SuiteGenerator.java
+++ src/test/java/SuiteGenerator.java
@@ -1,15 +1,24 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
 import io.github.classgraph.ClassGraph;
 import io.github.classgraph.ClassInfoList;
-import org.assertj.core.util.Arrays;
 import org.testng.TestNG;
 import org.testng.annotations.Test;
 import org.testng.xml.XmlClass;
 import org.testng.xml.XmlSuite;
+import org.testng.xml.XmlSuite.ParallelMode;
 import org.testng.xml.XmlTest;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 
 public class SuiteGenerator {
+    public SuiteGenerator() {
+    }
 
     public static void main(String[] args) {
         if (args[0] != null && args[1] == null) {
@@ -18,22 +27,18 @@ public class SuiteGenerator {
         } else {
             generateSuiteXML(args[0], args[1]);
         }
+
     }
 
     private static void generateSuiteXML(String threadCounts, String basePackage) {
         basePackage = System.getProperty("RunPackage") == null ? basePackage : System.getProperty("RunPackage");
         int threadCount = threadCounts == null ? 1 : Integer.valueOf(threadCounts);
-        XmlSuite.ParallelMode parallelMode = XmlSuite.ParallelMode.TESTS;
-
-
+        ParallelMode parallelMode = ParallelMode.TESTS;
         String suiteName = System.getProperty("SuiteName") == null ? "Regression" : System.getProperty("SuiteName");
         System.out.println(Thread.currentThread().getId() + ": !!!!!!! -- STARTING SUITE GENERATOR -- !!!!!!!");
-
         TestNG testNG = new TestNG();
         ClassGraph graph = new ClassGraph();
         ClassInfoList classesWithAnnotation = graph.whitelistPackages(basePackage).enableAllInfo().scan().getClassesWithMethodAnnotation(Test.class.getCanonicalName());
-
-        // Add Listener
         XmlSuite xmlSuite = new XmlSuite();
         xmlSuite.setName(suiteName + " Regression");
         xmlSuite.setVerbose(1);
@@ -45,36 +50,29 @@ public class SuiteGenerator {
         ArrayList<String> listeners = new ArrayList<>();
         listeners.add("framework.Listener");
         xmlSuite.setListeners(listeners);
-
-        classesWithAnnotation.forEach(classInfo -> {
-            // Add Test
+        classesWithAnnotation.forEach((classInfo) -> {
             XmlTest xmlTest = new XmlTest(xmlSuite);
             xmlTest.setName(classInfo.getSimpleName());
             xmlTest.setPreserveOrder(true);
             if (threadCount > 1) {
                 xmlTest.setParallel(parallelMode);
             }
-            xmlTest.setThreadCount(threadCount);
 
-            // Add Classes
+            xmlTest.setThreadCount(threadCount);
             LinkedList<XmlClass> classes = new LinkedList<>();
             XmlClass xmlClass = new XmlClass();
             xmlClass.setName(classInfo.getName());
             classes.add(xmlClass);
             xmlTest.setClasses(classes);
-
             if (Boolean.valueOf(System.getProperty("isClockMove"))) {
                 xmlTest.addIncludedGroup("ClockMove");
             } else {
                 xmlTest.addExcludedGroup("ClockMove");
             }
-        });
 
+        });
         testNG.setXmlSuites(Collections.singletonList(xmlSuite));
         System.out.println(xmlSuite.toXml());
         testNG.run();
-
     }
-
-
 }
