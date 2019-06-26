e77edb5bcfabc1a26e05f2ca3e9468e6234ba88c
Anurag Deshpande <adeshpande@idfbins.com>
2019-06-26 10:31:11 -0600

    fixing the missing timestamp on the generated report file.

--- src/main/java/framework/guidewire/gw8/elements/GWElement.java
+++ src/main/java/framework/guidewire/gw8/elements/GWElement.java
@@ -64,13 +64,13 @@ public class GWElement extends UIElement {
 
             // closing the warning window if Identifier is marked to check it
             if (identifier.shouldCheckForWarning()) {
-                closeWarningWindow();
                 try{
                     PauseTest.createSpecialInstance(3, 10).until(ExpectedConditions.visibilityOfElementLocated(By.id("southPanel")));
                 } catch (Exception e){
                     // do nothing. just the south panel did not show up. so no warning windows.
                 }
 
+                closeWarningWindow();
             }
 
             // checking for error messages after clicking
