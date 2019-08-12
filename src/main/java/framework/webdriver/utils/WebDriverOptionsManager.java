4acc69a964e5fcf3446c42c0ebbb3fc830de624f
Anurag Deshpande <adeshpande@idfbins.com>
2019-08-12 12:29:26 -0600

    Removing Collections decoration to pass the chrome arguments

--- src/main/java/framework/webdriver/utils/WebDriverOptionsManager.java
+++ src/main/java/framework/webdriver/utils/WebDriverOptionsManager.java
@@ -2,18 +2,16 @@ package framework.webdriver.utils;
 
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.Collections;
-
 public class WebDriverOptionsManager {
 
     public ChromeOptions getChromeOptions(){
         ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments(Collections.singletonList("--start-maximized"));
-        chromeOptions.addArguments(Collections.singletonList("--no-sandbox")); //https://stackoverflow.com/a/50725918/1689770
-        chromeOptions.addArguments(Collections.singletonList("--disable-infobars")); //https://stackoverflow.com/a/43840128/1689770
-        chromeOptions.addArguments(Collections.singletonList("--disable-dev-shm-usage")); //https://stackoverflow.com/a/50725918/1689770
-        chromeOptions.addArguments(Collections.singletonList("--disable-browser-side-navigation")); //https://stackoverflow.com/a/49123152/1689770
-        chromeOptions.addArguments(Collections.singletonList("--disable-gpu"));
+        chromeOptions.addArguments("--start-maximized");
+        chromeOptions.addArguments("--no-sandbox"); //https://stackoverflow.com/a/50725918/1689770
+        chromeOptions.addArguments("--disable-infobars"); //https://stackoverflow.com/a/43840128/1689770
+        chromeOptions.addArguments("--disable-dev-shm-usage"); //https://stackoverflow.com/a/50725918/1689770
+        chromeOptions.addArguments("--disable-browser-side-navigation"); //https://stackoverflow.com/a/49123152/1689770
+        chromeOptions.addArguments("--disable-gpu");
 //        chromeOptions.setUnhandledPromptBehaviour(UnexpectedAlertBehaviour.ACCEPT);
 
         return chromeOptions;
