35628a3609e5ea75227441a15aa97740509f3cec
Anurag Deshpande <adeshpande@idfbins.com>
2019-09-13 07:32:33 -0600

    Accessing VIN Numbers from a local DB Adding ability to create a local cache of 200 VIN numbers that auto replenishes when the stock has 20 or so VIN Numbers left.

--- src/main/java/framework/integrations/vin/VINTools.java
+++ src/main/java/framework/integrations/vin/VINTools.java
@@ -10,11 +10,11 @@ import java.net.URL;
 
 public class VINTools {
     public static String getRealVIN(){
-        return getVIN("real");
+        return StockVINNumbers.getRealVIN();
     }
 
     public static String getFakeValidVIN(){
-        return getVIN("fake");
+        return StockVINNumbers.getVIN("fake");
     }
 
     public static Vehicle decodeVIN(String vinNumber){
@@ -41,33 +41,6 @@ public class VINTools {
 
         try {
             url = new URL("https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVinValues/"+vinNumber+"?format=json");
-            is = url.openStream();  // throws an IOException
-            br = new BufferedReader(new InputStreamReader(is));
-
-            while ((line = br.readLine()) != null) {
-                return line;
-            }
-        } catch (IOException mue) {
-            mue.printStackTrace();
-        } finally {
-            try {
-                if (is != null) is.close();
-            } catch (IOException ioe) {
-                // nothing to see here
-            }
-        }
-
-        return null;
-    }
-
-    private static String getVIN(String type){
-        URL url;
-        InputStream is = null;
-        BufferedReader br;
-        String line;
-
-        try {
-            url = new URL("http://randomvin.com/getvin.php?type=" + type);
             is = url.openStream();  // throws an IOException
             br = new BufferedReader(new InputStreamReader(is));
 
