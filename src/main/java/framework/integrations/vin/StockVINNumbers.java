c07f89cd5e8405153bd01bef469c6f8135114055
Anurag Deshpande <adeshpande@idfbins.com>
2019-09-16 09:43:48 -0600

    Moving the cache to resources folder to make sure the cache is in the project files

--- src/main/java/framework/integrations/vin/StockVINNumbers.java
+++ src/main/java/framework/integrations/vin/StockVINNumbers.java
@@ -1,5 +1,8 @@
 package framework.integrations.vin;
 
+import framework.constants.ReactionTime;
+import framework.webdriver.BrowserFactory;
+
 import java.io.*;
 import java.net.URL;
 import java.util.ArrayList;
@@ -9,7 +12,7 @@ import java.util.Random;
 public class StockVINNumbers implements Serializable {
 
     private static void reloadVINs(){
-        System.out.println("Getting 200 New Vin numbers for stock");
+        System.out.println("Getting 50 New Vin numbers for stock");
         ArrayList<String> vins = new ArrayList<>();
         for (int i = 0; i < 200; i++) {
             vins.add(getVIN("Real"));
@@ -17,23 +20,28 @@ public class StockVINNumbers implements Serializable {
 
         writeVins(vins);
         System.out.println("Restocking VIN Complete.");
+       BrowserFactory.changeImplicitWaitTo(ReactionTime.STANDARD_WAIT_TIME);
     }
 
     public static String getRealVIN(){
-        List<String> vins = readVins();
-        String vinToReturn = vins.get(new Random().nextInt(vins.size()));
-        vins.remove(vinToReturn);
-        if(vins.size() < 50){
-            reloadVINs();
-        } else {
-            writeVins(vins);
-        }
+//        String vinToReturn = getVIN("Real");
+//        if(vinToReturn == null){
+            List<String> vins = readVins();
+            String vinToReturn = vins.get(new Random().nextInt(vins.size()));
+            vins.remove(vinToReturn);
+            if(vins.size() < 50){
+                reloadVINs();
+            } else {
+                writeVins(vins);
+            }
+//        }
+
 
         return vinToReturn;
     }
 
     private static void writeVins(List<String> vins){
-        try(FileOutputStream fos = new FileOutputStream("vins.tmp"); ObjectOutputStream oos = new ObjectOutputStream(fos)){
+        try(FileOutputStream fos = new FileOutputStream(StockVINNumbers.class.getResource("/").getPath()+"/vins.tmp"); ObjectOutputStream oos = new ObjectOutputStream(fos)){
             oos.writeObject(vins);
         } catch (IOException e) {
             e.printStackTrace();
@@ -41,7 +49,7 @@ public class StockVINNumbers implements Serializable {
     }
 
     private static List<String> readVins(){
-        try(FileInputStream fis = new FileInputStream("vins.tmp"); ObjectInputStream ois = new ObjectInputStream(fis)){
+        try(FileInputStream fis = new FileInputStream(StockVINNumbers.class.getResource("/").getPath()+"/vins.tmp"); ObjectInputStream ois = new ObjectInputStream(fis)){
             return (List<String>) ois.readObject();
         } catch (FileNotFoundException fnf){
             reloadVINs();
