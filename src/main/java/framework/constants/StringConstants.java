ba0a951860ae84eb6f64520983009bcb0b2c77aa
Anurag Deshpande <adeshpande@idfbins.com>
2019-09-17 08:01:52 -0600

    Adding String Constants to declare testName, testClassName, XMLTestName and SuiteName

--- src/main/java/framework/constants/StringConstants.java
+++ src/main/java/framework/constants/StringConstants.java
@@ -4,4 +4,8 @@ public final class StringConstants {
     public static final String BLANK = "";
     public static final String TAB = "\t";
     public static final String SPACE = " ";
+    public static final String XML_TEST_NAME = "xmlTestName_strvar";
+    public static final String TEST_CLASS_NAME = "testClassName_strvar";
+    public static final String TEST_NAME = "testName_strvar";
+    public static final String SUITE_NAME = "suiteName_strvar";
 }
