5d316030798c57e77a59a386e9c8afaa11edf901
Anurag Deshpande <adeshpande@idfbins.com>
2019-09-17 08:01:18 -0600

    Adding documentation for methods Adding static method for storing data in case the interact object is still not initiated and storage is needed

--- src/main/java/framework/webdriver/utils/BrowserStorageAccess.java
+++ src/main/java/framework/webdriver/utils/BrowserStorageAccess.java
@@ -13,6 +13,9 @@ public class BrowserStorageAccess {
 
     /**
      * Uses local browser storage to store a key value pair.
+     *
+     * <b><u>IF A KEY ALREADY EXISTS, IT WILL BE OVERRIDDEN</u></b>
+     *
      * Complex data values can be serialized to String
      *
      *  1. Browser storage is synchronized, multiple/ simultaneous calls to storage will be
@@ -20,6 +23,8 @@ public class BrowserStorageAccess {
  *     2. DO NOT store sensitive information in the storage
  *     3. This storage has little to no form of data protection
  *     4. Can store only 5MB of data in totality
+     *
+     * <b>ANY STORED VARIABLES WILL BE DROPPED AS SOON AS THE BROWSER IS CLOSED</b>
      *
      * @param key unique key in the store
      * @param value value associated with the key
@@ -45,12 +50,26 @@ public class BrowserStorageAccess {
         executor.executeScript("window.localStorage.removeItem('"+key+"')");
     }
 
-    public synchronized void clearStorage(){
-        executor.executeScript("window.localStorage.clear()");
-    }
-
-    public static synchronized void clearAllStorage(){
-        ((JavascriptExecutor) BrowserFactory.getCurrentBrowser().getDriver()).executeScript("window.localStorage.clear()");
+    /**
+     * Uses local browser storage to store a key value pair.
+     *
+     * <b><u>IF A KEY ALREADY EXISTS, IT WILL BE OVERRIDDEN</u></b>
+     *
+     * Complex data values can be serialized to String
+     *
+     *  1. Browser storage is synchronized, multiple/ simultaneous calls to storage will be
+     *    executed sequentially
+     *     2. DO NOT store sensitive information in the storage
+     *     3. This storage has little to no form of data protection
+     *     4. Can store only 5MB of data in totality
+     *
+     * <b>ANY STORED VARIABLES WILL BE DROPPED AS SOON AS THE BROWSER IS CLOSED</b>
+     *
+     * @param key unique key in the store
+     * @param value value associated with the key
+     */
+    public static synchronized void storeData(String key, String value){
+        ((JavascriptExecutor) BrowserFactory.getCurrentBrowser().getDriver()).executeScript("window.localStorage.setItem('"+ key+"', '"+value+"')");
     }
 
 }
