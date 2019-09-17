0044982005ab833874f572ed9ce36089141735b5
Anurag Deshpande <adeshpande@idfbins.com>
2019-09-17 08:50:50 -0600

    removing static duplicate methods and allowing static access via createInstance() method

--- src/main/java/framework/webdriver/utils/BrowserStorageAccess.java
+++ src/main/java/framework/webdriver/utils/BrowserStorageAccess.java
@@ -50,26 +50,7 @@ public class BrowserStorageAccess {
         executor.executeScript("window.localStorage.removeItem('"+key+"')");
     }
 
-    /**
-     * Uses local browser storage to store a key value pair.
-     *
-     * <b><u>IF A KEY ALREADY EXISTS, IT WILL BE OVERRIDDEN</u></b>
-     *
-     * Complex data values can be serialized to String
-     *
-     *  1. Browser storage is synchronized, multiple/ simultaneous calls to storage will be
-     *    executed sequentially
-     *     2. DO NOT store sensitive information in the storage
-     *     3. This storage has little to no form of data protection
-     *     4. Can store only 5MB of data in totality
-     *
-     * <b>ANY STORED VARIABLES WILL BE DROPPED AS SOON AS THE BROWSER IS CLOSED</b>
-     *
-     * @param key unique key in the store
-     * @param value value associated with the key
-     */
-    public static synchronized void storeData(String key, String value){
-        ((JavascriptExecutor) BrowserFactory.getCurrentBrowser().getDriver()).executeScript("window.localStorage.setItem('"+ key+"', '"+value+"')");
+    public static BrowserStorageAccess getInstance(){
+        return new BrowserStorageAccess();
     }
-
 }
