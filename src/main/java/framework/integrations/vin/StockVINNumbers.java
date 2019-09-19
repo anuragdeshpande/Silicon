356cf37c6d67f3ab37c67f0fc6d5db43d7728f6d
Anurag Deshpande <adeshpande@idfbins.com>
2019-09-19 13:34:04 -0600

    Refactoring VINTools to use the new HTTPGet Library

--- src/main/java/framework/integrations/vin/StockVINNumbers.java
+++ src/main/java/framework/integrations/vin/StockVINNumbers.java
@@ -1,10 +1,10 @@
 package framework.integrations.vin;
 
 import framework.constants.ReactionTime;
+import framework.integrations.http.HTTPGet;
 import framework.webdriver.BrowserFactory;
 
 import java.io.*;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -61,29 +61,6 @@ public class StockVINNumbers implements Serializable {
     }
 
     static String getVIN(String type){
-        URL url;
-        InputStream is = null;
-        BufferedReader br;
-        String line;
-
-        try {
-            url = new URL("http://randomvin.com/getvin.php?type=" + type);
-            is = url.openStream();  // throws an IOException
-            br = new BufferedReader(new InputStreamReader(is));
-
-            while ((line = br.readLine()) != null) {
-                return line;
-            }
-        } catch (IOException mue) {
-            mue.printStackTrace();
-        } finally {
-            try {
-                if (is != null) is.close();
-            } catch (IOException ioe) {
-                // nothing to see here
-            }
-        }
-
-        return null;
+        return HTTPGet.makeGETRequest("http://randomvin.com/getvin.php?type=" + type);
     }
 }
