1280a9ebc9f8761371c3b7a420e98a179361fdcb
Anurag Deshpande <adeshpande@idfbins.com>
2019-09-19 13:38:08 -0600

    Adding Banner messages while reStocking the VINs Refactoring to correct access modifiers in the class

--- src/main/java/framework/integrations/vin/StockVINNumbers.java
+++ src/main/java/framework/integrations/vin/StockVINNumbers.java
@@ -9,27 +9,28 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class StockVINNumbers implements Serializable {
+class StockVINNumbers implements Serializable {
 
-    private static void reloadVINs(){
-        System.out.println("Getting 50 New Vin numbers for stock");
+    private static void reloadVINs() {
+        BrowserFactory.getCurrentBrowser().withDOM().injectInfoMessage("Getting 50 New Vin numbers for stock");
         ArrayList<String> vins = new ArrayList<>();
         for (int i = 0; i < 200; i++) {
             vins.add(getVIN("Real"));
         }
 
         writeVins(vins);
-        System.out.println("Restocking VIN Complete.");
-       BrowserFactory.changeImplicitWaitTo(ReactionTime.STANDARD_WAIT_TIME);
+        BrowserFactory.getCurrentBrowser().withDOM().injectSuccessMessage("Restocking VIN Complete");
+        BrowserFactory.getCurrentBrowser().withDOM().clearBannerMessage();
+        BrowserFactory.changeImplicitWaitTo(ReactionTime.STANDARD_WAIT_TIME);
     }
 
-    public static String getRealVIN(){
+    static String getRealVIN() {
         String vinToReturn = getVIN("Real");
-        if(vinToReturn == null){
+        if (vinToReturn == null) {
             List<String> vins = readVins();
             vinToReturn = vins.get(new Random().nextInt(vins.size()));
             vins.remove(vinToReturn);
-            if(vins.size() < 50){
+            if (vins.size() < 50) {
                 reloadVINs();
             } else {
                 writeVins(vins);
@@ -40,27 +41,26 @@ public class StockVINNumbers implements Serializable {
         return vinToReturn;
     }
 
-    private static void writeVins(List<String> vins){
-        try(FileOutputStream fos = new FileOutputStream(StockVINNumbers.class.getResource("/").getPath()+"/vins.tmp"); ObjectOutputStream oos = new ObjectOutputStream(fos)){
+    private static void writeVins(List<String> vins) {
+        try (FileOutputStream fos = new FileOutputStream(StockVINNumbers.class.getResource("/").getPath() + "/vins.tmp"); ObjectOutputStream oos = new ObjectOutputStream(fos)) {
             oos.writeObject(vins);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static List<String> readVins(){
-        try(FileInputStream fis = new FileInputStream(StockVINNumbers.class.getResource("/").getPath()+"/vins.tmp"); ObjectInputStream ois = new ObjectInputStream(fis)){
+    private static List<String> readVins() {
+        try (FileInputStream fis = new FileInputStream(StockVINNumbers.class.getResource("/").getPath() + "/vins.tmp"); ObjectInputStream ois = new ObjectInputStream(fis)) {
             return (List<String>) ois.readObject();
-        } catch (FileNotFoundException fnf){
+        } catch (FileNotFoundException fnf) {
             reloadVINs();
             return readVins();
-        }
-        catch (IOException | ClassNotFoundException e) {
+        } catch (IOException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
 
-    static String getVIN(String type){
+    static String getVIN(String type) {
         return HTTPGet.makeGETRequest("http://randomvin.com/getvin.php?type=" + type);
     }
 }
