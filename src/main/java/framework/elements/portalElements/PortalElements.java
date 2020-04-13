364b66fefd7ebaa1e9649f082296b6784d1e56d7
Anurag Deshpande <adeshpande@idfbins.com>
2020-04-13 07:42:37 -0600

    removing references to button standardization from portal elements class

--- src/main/java/framework/elements/portalElements/PortalElements.java
+++ src/main/java/framework/elements/portalElements/PortalElements.java
@@ -5,14 +5,6 @@ import framework.elements.enums.ElementType;
 import org.openqa.selenium.By;
 
 public class PortalElements {
-    public static Identifier button(String buttonName){
-        By byReference = By.xpath("//*[contains(@class, 'gw-btn-primary') and contains(text(), '"+buttonName+"')]");
-        return new Identifier(byReference, ElementType.BUTTON);
-    }
-
-    public static Identifier link(String linkText){
-        return new Identifier(By.linkText(linkText), ElementType.LINK);
-    }
 
     public static Identifier table(String valueOfListAttributeOnTableElement){
         return new Identifier(By.xpath("//table[@list='"+valueOfListAttributeOnTableElement+"']"), ElementType.TABLE);
