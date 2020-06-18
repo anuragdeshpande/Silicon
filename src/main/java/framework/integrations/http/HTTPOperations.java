4cc76c405bf8eeef63148b7cd52f1906041ec6e0
Anurag Deshpande <adeshpande@idfbins.com>
2020-06-18 13:26:30 -0600

    Refactoring class and optimizing code

--- src/main/java/framework/integrations/http/HTTPOperations.java
+++ src/main/java/framework/integrations/http/HTTPOperations.java
@@ -2,16 +2,14 @@ package framework.integrations.http;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class HTTPGet {
+public class HTTPOperations {
 
-    public static synchronized String makeGETRequest(String getURL){
+    public static synchronized String makeGETRequest(String getURL) {
         URL url;
-        InputStream is = null;
         BufferedReader br;
         String line;
 
@@ -21,8 +19,7 @@ public class HTTPGet {
             con.setRequestMethod("GET");
             con.setRequestProperty("User-Agent", "Mozilla/5.0");
             int responseCode = con.getResponseCode();
-            System.out.println("GET Response Code: "+responseCode);
-            if(responseCode == HttpURLConnection.HTTP_OK){
+            if (responseCode == HttpURLConnection.HTTP_OK) {
                 br = new BufferedReader(new InputStreamReader(con.getInputStream()));
                 StringBuilder response = new StringBuilder();
                 while ((line = br.readLine()) != null) {
