b25fc6fc2192598d01a67224577ee51bc643a3a1
Anurag Deshpande <adeshpande@idfbins.com>
2020-09-26 08:01:58 -0600

    reading required values from system properties instead of arguments from main method

--- src/test/java/CombineReports.java
+++ src/test/java/CombineReports.java
@@ -1,10 +1,11 @@
 import framework.ReportManager;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 public class CombineReports {
     public static void main(String[] args) throws IOException {
-        ReportManager.generateCombinedReports(args[0], Arrays.copyOfRange(args, 1, args.length));
+        String targetLocation = System.getProperty("targetReportLocation");
+        String[] reportFileLocations = System.getProperty("reportFileLocations", "").split(",");
+        ReportManager.generateCombinedReports(targetLocation, reportFileLocations);
     }
 }
