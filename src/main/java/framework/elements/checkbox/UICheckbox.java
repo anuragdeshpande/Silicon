048aca83af58bf6c37d83799f55effd4ee96610e
Anurag Deshpande <adeshpande@idfbins.com>
2020-11-12 07:13:38 -0700

    refactoring logic to use standard web element for performing interactions

--- src/main/java/framework/elements/checkbox/UICheckbox.java
+++ src/main/java/framework/elements/checkbox/UICheckbox.java
@@ -3,9 +3,7 @@ package framework.elements.checkbox;
 import framework.constants.ReactionTime;
 import framework.elements.Identifier;
 import framework.elements.ui_element.UIElement;
-import framework.webdriver.BrowserFactory;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Action;
 
 public class UICheckbox extends UIElement implements IUICheckbox {
 
@@ -21,19 +19,8 @@ public class UICheckbox extends UIElement implements IUICheckbox {
         super(identifier, reactionTime);
     }
 
-    @Override
-    public void click() {
-        Action clickCheckbox = BrowserFactory.getCurrentBrowser().getActions().clickAndHold(this.getElement())
-                .moveByOffset(1, 1)
-                .release(this.getElement())
-                .build();
-        clickCheckbox.perform();
-//        System.out.println("Clicking Checkbox");
-    }
-
     public boolean isChecked() {
-        //        System.out.println("Is Checkbox Checked: "+isChecked);
-        return this.getElement().getAttribute("class").contains("checked");
+        return this.getElement().isSelected();
     }
 
     @Override
