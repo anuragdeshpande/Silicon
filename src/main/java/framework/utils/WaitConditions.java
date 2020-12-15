de2c4d6a11cbea0ef53a32135ab8abd40847a07e
Anurag Deshpande <adeshpande@idfbins.com>
2020-12-15 04:00:25 -0700

    mandating message during waits for all custom wait calls

---
+++ src/main/java/framework/utils/WaitConditions.java
@@ -0,0 +1,44 @@
+package framework.utils;
+
+import framework.logger.RegressionLogger;
+import framework.webdriver.BrowserFactory;
+import framework.webdriver.Interact;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.function.Function;
+
+public class WaitConditions {
+    private WebDriver driver;
+    private long timeoutInSeconds;
+    private long pollingIntervalMilliseconds;
+    private WebDriverWait wait;
+
+    public WaitConditions(WebDriver driver, long timeoutInSeconds, long pollingIntervalMilliseconds){
+        this.driver = driver;
+        this.timeoutInSeconds = timeoutInSeconds;
+        this.pollingIntervalMilliseconds = pollingIntervalMilliseconds;
+
+        this.wait = new WebDriverWait(driver, timeoutInSeconds);
+    }
+
+    public WebDriverWait getWebdriverWait() {
+        return wait;
+    }
+
+    public <V> V until(Function<? super WebDriver, V> waitCondition, String messageToShowWhileWaiting){
+        Interact interact = BrowserFactory.getCurrentBrowser();
+        interact.withDOM().injectInfoMessage(messageToShowWhileWaiting);
+        try{
+            V until = wait.until(waitCondition);
+            interact.withDOM().clearBannerMessage();
+            return until;
+        } catch (TimeoutException toe){
+            interact.withDOM().injectDangerMessage("Page did not load in under "+timeoutInSeconds+" seconds");
+            RegressionLogger.getTestLogger().captureScreenshot("Page is still loading");
+            RegressionLogger.getTestLogger().captureScreenshot("Page loading did not meet SLA");
+            throw new TimeoutException("Page is still loading");
+        }
+    }
+}
