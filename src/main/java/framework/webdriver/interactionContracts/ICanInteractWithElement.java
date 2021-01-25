eda76bf3393d44f5dd632097a2d3325e65f4a4df
Anurag Deshpande <adeshpande@idfbins.com>
2021-01-25 11:31:59 -0700

    optimizing imports

--- src/main/java/framework/webdriver/interactionContracts/ICanInteractWithElement.java
+++ src/main/java/framework/webdriver/interactionContracts/ICanInteractWithElement.java
@@ -3,6 +3,8 @@ package framework.webdriver.interactionContracts;
 import framework.constants.ReactionTime;
 import framework.elements.Identifier;
 
+import java.util.Optional;
+
 public interface ICanInteractWithElement<T> {
     T withElement(Identifier identifier);
     T withOptionalElement(Identifier identifier, ReactionTime reactionTime);
