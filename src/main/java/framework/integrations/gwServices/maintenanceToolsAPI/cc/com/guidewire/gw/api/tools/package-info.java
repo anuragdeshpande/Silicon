7effdb2348aba81037bf419d6c1c2d27e65669e6
adeshpande <adeshpande@idfbins.com>
2021-04-08 13:47:01 -0600

    removing cc maintenance tools api

--- src/main/java/framework/integrations/gwServices/maintenanceToolsAPI/cc/com/guidewire/gw/api/tools/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://guidewire.com/gw/api/tools", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package framework.integrations.gwServices.maintenanceToolsAPI.cc.com.guidewire.gw.api.tools;
--- src/main/java/framework/integrations/gwServices/maintenanceToolsAPI/cc/com/guidewire/gw/api/webservice/maintenancetools/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://guidewire.com/gw/api/webservice/maintenanceTools", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package framework.integrations.gwServices.maintenanceToolsAPI.cc.com.guidewire.gw.api.webservice.maintenancetools;
