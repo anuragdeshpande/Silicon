a21864ed0c51c6345365e5587d49ea1a3f44e515
adeshpande <adeshpande@idfbins.com>
2021-04-09 10:22:28 -0600

    Adding CC10 MaintenanceToolsAPI

---
+++ src/main/java/framework/integrations/gwServices/maintenanceToolsAPI/cc/ScheduleAggLimitRebuildInvalidLimitsResponse.java
@@ -0,0 +1,60 @@
+
+package framework.integrations.gwServices.maintenanceToolsAPI.cc;
+
+import javax.xml.bind.annotation.*;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="return" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "_return"
+})
+@XmlRootElement(name = "scheduleAggLimitRebuildInvalidLimitsResponse")
+public class ScheduleAggLimitRebuildInvalidLimitsResponse {
+
+    @XmlElement(name = "return")
+    protected String _return;
+
+    /**
+     * Gets the value of the return property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getReturn() {
+        return _return;
+    }
+
+    /**
+     * Sets the value of the return property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setReturn(String value) {
+        this._return = value;
+    }
+
+}
