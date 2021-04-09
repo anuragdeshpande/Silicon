540dc88b73a030e4c9e46100d23b4355efc91572
adeshpande <adeshpande@idfbins.com>
2021-04-09 10:37:46 -0600

    re-adding messaging tools API for CC10

---
+++ src/main/java/framework/integrations/gwServices/messagingToolsAPI/cc/com/guidewire/cc/ws/gw/webservice/cc/messagingtoolsapi/IsResumed.java
@@ -0,0 +1,83 @@
+
+package framework.integrations.gwServices.messagingToolsAPI.cc.com.guidewire.cc.ws.gw.webservice.cc.messagingtoolsapi;
+
+import framework.integrations.gwServices.messagingToolsAPI.cc.com.guidewire.gw.api.messaging.MessageProcessingDirection;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="destID" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="direction" type="{http://guidewire.com/gw/api/messaging}MessageProcessingDirection" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "destID",
+    "direction"
+})
+@XmlRootElement(name = "isResumed")
+public class IsResumed {
+
+    protected int destID;
+    protected MessageProcessingDirection direction;
+
+    /**
+     * Gets the value of the destID property.
+     * 
+     */
+    public int getDestID() {
+        return destID;
+    }
+
+    /**
+     * Sets the value of the destID property.
+     * 
+     */
+    public void setDestID(int value) {
+        this.destID = value;
+    }
+
+    /**
+     * Gets the value of the direction property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link MessageProcessingDirection }
+     *     
+     */
+    public MessageProcessingDirection getDirection() {
+        return direction;
+    }
+
+    /**
+     * Sets the value of the direction property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link MessageProcessingDirection }
+     *     
+     */
+    public void setDirection(MessageProcessingDirection value) {
+        this.direction = value;
+    }
+
+}
