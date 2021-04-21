40accdc8bc2188b584e5ea6b38504fbb2ec006b9
adeshpande <adeshpande@idfbins.com>
2021-04-21 08:29:06 -0600

    removing unnecessary methods

--- src/main/java/framework/webdriver/utils/WaitUtils.java
+++ src/main/java/framework/webdriver/utils/WaitUtils.java
@@ -8,26 +8,14 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WaitUtils {
 
-    private WebDriver driver;
-    private int defaultWait = 5;
+    private final WebDriver driver;
 
     public WaitUtils(WebDriver driver) {
         this.driver = driver;
     }
 
-    protected WebDriverWait newWait() {
-        return new WebDriverWait(this.driver, this.defaultWait);
-    }
-
     protected WebDriverWait newWait(int waitInSeconds) {
-        return new WebDriverWait(this.driver, timeFixer(waitInSeconds), 100);
-    }
-
-    protected int timeFixer(int time) {
-    	int tmp = (time > 300 && time < 1000) ? 1000 : time;
-    	tmp = tmp % 1000 == 0 ? (time / 1000 == 0 ? 1 : time / 1000) : tmp;
-//      System.out.println("Wait up to: " + tmp + "s");
-    	return tmp;
+        return new WebDriverWait(this.driver, waitInSeconds, 10);
     }
 
     public WebElement waitUntilElementIsClickable(By locator, int waitInSeconds) {
