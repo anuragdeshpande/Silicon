79e7501b7c60a4958b65c2523e776c4a90b08786
adeshpande <adeshpande@idfbins.com>
2021-05-03 11:19:42 -0600

    adding qa10 environment to the list

--- src/main/java/framework/enums/Environments.java
+++ src/main/java/framework/enums/Environments.java
@@ -4,7 +4,7 @@ public enum Environments {
     DEV, DEV2, DEV3, DEV10,
     IT,
     PS01, PS02, PS03,
-    QA, QA3,
+    QA, QA3, QA10,
     REGR01, REGR02, REGR03, REGR04, REGR05, REGR06, REGR07,
     STAB01, STAB02, STAB03, STAB04, STAB05,
     UAT, LOCAL;
