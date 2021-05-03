765d06ce02ed87831f6af8c7f67e47b3d0b3849f
adeshpande <adeshpande@idfbins.com>
2021-05-03 13:18:57 -0600

    removing MaintenanceToolsAPI from the framework and moving it to the main project

--- src/main/java/framework/integrations/gwServices/maintenanceToolsAPI/bc/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://guidewire.com/bc/ws/gw/webservice/bc/MaintenanceToolsAPI", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package framework.integrations.gwServices.maintenanceToolsAPI.bc;
--- src/main/java/framework/integrations/gwServices/maintenanceToolsAPI/pc/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://guidewire.com/pc/ws/gw/webservice/pc/pc1000/MaintenanceToolsAPI", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package framework.integrations.gwServices.maintenanceToolsAPI.pc;
