69725299a499c533146aa2decf8a64dfd4c24153
adeshpande <adeshpande@idfbins.com>
2021-05-04 12:44:09 -0600

    adding ability to change the resolution time for element and page loading at the identifier level

--- src/main/java/framework/elements/Identifier.java
+++ src/main/java/framework/elements/Identifier.java
@@ -1,6 +1,8 @@
 package framework.elements;
 
 import framework.elements.enums.ElementType;
+import framework.logger.RegressionLogger;
+import framework.webdriver.DriverFactory;
 import org.openqa.selenium.By;
 
 public class Identifier extends GenericIdentifier {
@@ -8,29 +10,33 @@ public class Identifier extends GenericIdentifier {
     private boolean canThrowWarning;
     private String friendlyName;
     private boolean ignoreTableLVCheck = true;
+    private int timeoutInSeconds;
 
     protected Identifier(){
-
+        setDefaultTimeout();
     }
 
     protected Identifier(By reference, ElementType type){
-
+        setDefaultTimeout();
     }
 
     public Identifier(By reference) {
         canThrowWarning = false;
         this.reference = reference;
+        setDefaultTimeout();
     }
 
     public Identifier(By reference, String friendlyName){
         this.reference = reference;
         this.friendlyName = friendlyName;
         this.canThrowWarning = false;
+        setDefaultTimeout();
     }
 
     public Identifier(By reference, String friendlyName, boolean checkForWarning){
         this(reference, friendlyName);
         this.canThrowWarning = checkForWarning;
+        setDefaultTimeout();
     }
 
     public boolean shouldCheckForWarning() {
@@ -62,4 +68,22 @@ public class Identifier extends GenericIdentifier {
     public String getReferenceValue(){
         return (this.reference.toString().split(":")[1]).trim();
     }
+
+    public int getTimeout(){
+        return this.timeoutInSeconds;
+    }
+
+    public Identifier setTimeoutInSeconds(int newTimeoutInSeconds){
+        RegressionLogger testLogger = RegressionLogger.getTestLogger();
+        testLogger.addTag("Custom_Timeout");
+        testLogger.info("Default timeout is being changed from: "+getTimeout()+" seconds to: "+newTimeoutInSeconds+" seconds");
+        this.timeoutInSeconds = newTimeoutInSeconds;
+        return this;
+    }
+
+    private void setDefaultTimeout(){
+        this.timeoutInSeconds = ((int) DriverFactory.getReactionTime().getTime());
+    }
+
+
 }
