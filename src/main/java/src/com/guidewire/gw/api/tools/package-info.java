55887eeb82872906e71ebadd928ac5836524a604
Anurag Deshpande <adeshpande@idfbins.com>
2021-05-27 08:35:28 -0600

    removing maintenance tools stubs from the framework as they are maintained in the main project
    Signed-off-by: anura <adeshpande@idfbins.com>

--- src/main/java/src/com/guidewire/gw/api/tools/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://guidewire.com/gw/api/tools", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.guidewire.gw.api.tools;
--- src/main/java/src/com/guidewire/gw/api/webservice/maintenancetools/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://guidewire.com/gw/api/webservice/maintenanceTools", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.guidewire.gw.api.webservice.maintenancetools;
