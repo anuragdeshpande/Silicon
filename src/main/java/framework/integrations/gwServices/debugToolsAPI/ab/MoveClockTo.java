6763032d6dbc3a6e68a66fc645acadcb35de5d24
adeshpande <adeshpande@idfbins.com>
2021-06-03 13:45:45 -0600

    rebuilding AB10 debug tools

--- src/main/java/framework/integrations/gwServices/debugToolsAPI/ab/MoveClockTo.java
+++ src/main/java/framework/integrations/gwServices/debugToolsAPI/ab/MoveClockTo.java
@@ -11,15 +11,15 @@ import javax.xml.datatype.XMLGregorianCalendar;
  * <p>The following schema fragment specifies the expected content contained within this class.
  * 
  * <pre>
- * &lt;complexType>
- *   &lt;complexContent>
- *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
- *       &lt;sequence>
- *         &lt;element name="date" type="{http://www.w3.org/2001/XMLSchema}dateTime" minOccurs="0"/>
- *       &lt;/sequence>
- *     &lt;/restriction>
- *   &lt;/complexContent>
- * &lt;/complexType>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="date" type="{http://www.w3.org/2001/XMLSchema}dateTime" minOccurs="0"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
  * </pre>
  * 
  * 
--- src/main/java/framework/integrations/gwServices/debugToolsAPI/ab/ObjectFactory.java
+++ src/main/java/framework/integrations/gwServices/debugToolsAPI/ab/ObjectFactory.java
@@ -1,13 +1,16 @@
 
 package framework.integrations.gwServices.debugToolsAPI.ab;
 
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlElementDecl;
 import javax.xml.bind.annotation.XmlRegistry;
+import javax.xml.namespace.QName;
 
 
 /**
  * This object contains factory methods for each 
  * Java content interface and Java element interface 
- * generated in the gwclockservice.abdebugtoolsapi package. 
+ * generated in the framework.integrations.gwServices.debugToolsAPI.ab package. 
  * <p>An ObjectFactory allows you to programatically 
  * construct new instances of the Java representation 
  * for XML content. The Java representation of XML 
@@ -20,11 +23,40 @@ import javax.xml.bind.annotation.XmlRegistry;
  */
 @XmlRegistry
 public class ObjectFactory {
+
+    private final static QName _Authentication_QNAME = new QName("http://guidewire.com/ws/soapheaders", "authentication");
+    private final static QName _Locale_QNAME = new QName("http://guidewire.com/ws/soapheaders", "locale");
+    private final static QName _TransactionId_QNAME = new QName("http://guidewire.com/ws/soapheaders", "transaction_id");
+
     /**
-     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: gwclockservice.abdebugtoolsapi
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: framework.integrations.gwServices.debugToolsAPI.ab
      * 
      */
     public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link Authentication }
+     * 
+     */
+    public Authentication createAuthentication() {
+        return new Authentication();
+    }
+
+    /**
+     * Create an instance of {@link Locale }
+     * 
+     */
+    public Locale createLocale() {
+        return new Locale();
+    }
+
+    /**
+     * Create an instance of {@link TransactionId }
+     * 
+     */
+    public TransactionId createTransactionId() {
+        return new TransactionId();
     }
 
     /**
@@ -36,35 +68,11 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link IsMembershipDuesUpdateRunningResponse }
-     * 
-     */
-    public IsMembershipDuesUpdateRunningResponse createIsMembershipDuesUpdateRunningResponse() {
-        return new IsMembershipDuesUpdateRunningResponse();
-    }
-
-    /**
      * Create an instance of {@link MoveClockToResponse }
      * 
      */
     public MoveClockToResponse createMoveClockToResponse() {
         return new MoveClockToResponse();
-    }
-
-    /**
-     * Create an instance of {@link RunMembershipDuesUpdate }
-     * 
-     */
-    public RunMembershipDuesUpdate createRunMembershipDuesUpdate() {
-        return new RunMembershipDuesUpdate();
-    }
-
-    /**
-     * Create an instance of {@link GetCurrentDate }
-     * 
-     */
-    public GetCurrentDate createGetCurrentDate() {
-        return new GetCurrentDate();
     }
 
     /**
@@ -76,19 +84,11 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link RunMembershipDuesUpdateResponse }
+     * Create an instance of {@link GetCurrentDate }
      * 
      */
-    public RunMembershipDuesUpdateResponse createRunMembershipDuesUpdateResponse() {
-        return new RunMembershipDuesUpdateResponse();
-    }
-
-    /**
-     * Create an instance of {@link IsMembershipDuesUpdateRunning }
-     * 
-     */
-    public IsMembershipDuesUpdateRunning createIsMembershipDuesUpdateRunning() {
-        return new IsMembershipDuesUpdateRunning();
+    public GetCurrentDate createGetCurrentDate() {
+        return new GetCurrentDate();
     }
 
     /**
@@ -98,4 +98,60 @@ public class ObjectFactory {
     public GetCurrentDateResponse createGetCurrentDateResponse() {
         return new GetCurrentDateResponse();
     }
+
+    /**
+     * Create an instance of {@link RunMembershipDuesUpdate }
+     * 
+     */
+    public RunMembershipDuesUpdate createRunMembershipDuesUpdate() {
+        return new RunMembershipDuesUpdate();
+    }
+
+    /**
+     * Create an instance of {@link RunMembershipDuesUpdateResponse }
+     * 
+     */
+    public RunMembershipDuesUpdateResponse createRunMembershipDuesUpdateResponse() {
+        return new RunMembershipDuesUpdateResponse();
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Authentication }{@code >}
+     * 
+     * @param value
+     *     Java instance representing xml element's value.
+     * @return
+     *     the new instance of {@link JAXBElement }{@code <}{@link Authentication }{@code >}
+     */
+    @XmlElementDecl(namespace = "http://guidewire.com/ws/soapheaders", name = "authentication")
+    public JAXBElement<Authentication> createAuthentication(Authentication value) {
+        return new JAXBElement<Authentication>(_Authentication_QNAME, Authentication.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Locale }{@code >}
+     * 
+     * @param value
+     *     Java instance representing xml element's value.
+     * @return
+     *     the new instance of {@link JAXBElement }{@code <}{@link Locale }{@code >}
+     */
+    @XmlElementDecl(namespace = "http://guidewire.com/ws/soapheaders", name = "locale")
+    public JAXBElement<Locale> createLocale(Locale value) {
+        return new JAXBElement<Locale>(_Locale_QNAME, Locale.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link TransactionId }{@code >}
+     * 
+     * @param value
+     *     Java instance representing xml element's value.
+     * @return
+     *     the new instance of {@link JAXBElement }{@code <}{@link TransactionId }{@code >}
+     */
+    @XmlElementDecl(namespace = "http://guidewire.com/ws/soapheaders", name = "transaction_id")
+    public JAXBElement<TransactionId> createTransactionId(TransactionId value) {
+        return new JAXBElement<TransactionId>(_TransactionId_QNAME, TransactionId.class, null, value);
+    }
+
 }
