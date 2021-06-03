3dd97fca95db5e84a6cb53577e5ff945df8126ac
adeshpande <adeshpande@idfbins.com>
2021-06-03 13:55:06 -0600

    rebuilding CC10 debug tools

--- src/main/java/framework/integrations/gwServices/debugToolsAPI/cc/ObjectFactory.java
+++ src/main/java/framework/integrations/gwServices/debugToolsAPI/cc/ObjectFactory.java
@@ -1,13 +1,16 @@
 
 package framework.integrations.gwServices.debugToolsAPI.cc;
 
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlElementDecl;
 import javax.xml.bind.annotation.XmlRegistry;
+import javax.xml.namespace.QName;
 
 
 /**
  * This object contains factory methods for each 
  * Java content interface and Java element interface 
- * generated in the gwclockservice.ccdebugtoolsapi package. 
+ * generated in the framework.integrations.gwServices.debugToolsAPI.cc package. 
  * <p>An ObjectFactory allows you to programatically 
  * construct new instances of the Java representation 
  * for XML content. The Java representation of XML 
@@ -20,11 +23,40 @@ import javax.xml.bind.annotation.XmlRegistry;
  */
 @XmlRegistry
 public class ObjectFactory {
+
+    private final static QName _Authentication_QNAME = new QName("http://guidewire.com/ws/soapheaders", "authentication");
+    private final static QName _Locale_QNAME = new QName("http://guidewire.com/ws/soapheaders", "locale");
+    private final static QName _TransactionId_QNAME = new QName("http://guidewire.com/ws/soapheaders", "transaction_id");
+
     /**
-     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: gwclockservice.ccdebugtoolsapi
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: framework.integrations.gwServices.debugToolsAPI.cc
      * 
      */
     public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link Authentication }
+     * 
+     */
+    public Authentication createAuthentication() {
+        return new Authentication();
+    }
+
+    /**
+     * Create an instance of {@link Locale }
+     * 
+     */
+    public Locale createLocale() {
+        return new Locale();
+    }
+
+    /**
+     * Create an instance of {@link TransactionId }
+     * 
+     */
+    public TransactionId createTransactionId() {
+        return new TransactionId();
     }
 
     /**
@@ -36,67 +68,11 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link GetCurrentTimeResponse }
-     * 
-     */
-    public GetCurrentTimeResponse createGetCurrentTimeResponse() {
-        return new GetCurrentTimeResponse();
-    }
-
-    /**
      * Create an instance of {@link MoveClockToResponse }
      * 
      */
     public MoveClockToResponse createMoveClockToResponse() {
         return new MoveClockToResponse();
-    }
-
-    /**
-     * Create an instance of {@link IsFinancialEscRunning }
-     * 
-     */
-    public IsFinancialEscRunning createIsFinancialEscRunning() {
-        return new IsFinancialEscRunning();
-    }
-
-    /**
-     * Create an instance of {@link AddDaysResponse }
-     * 
-     */
-    public AddDaysResponse createAddDaysResponse() {
-        return new AddDaysResponse();
-    }
-
-    /**
-     * Create an instance of {@link IsFinancialEscRunningResponse }
-     * 
-     */
-    public IsFinancialEscRunningResponse createIsFinancialEscRunningResponse() {
-        return new IsFinancialEscRunningResponse();
-    }
-
-    /**
-     * Create an instance of {@link IsMainframeOlieCheckFileRunning }
-     * 
-     */
-    public IsMainframeOlieCheckFileRunning createIsMainframeOlieCheckFileRunning() {
-        return new IsMainframeOlieCheckFileRunning();
-    }
-
-    /**
-     * Create an instance of {@link GetCurrentTime }
-     * 
-     */
-    public GetCurrentTime createGetCurrentTime() {
-        return new GetCurrentTime();
-    }
-
-    /**
-     * Create an instance of {@link AddDays }
-     * 
-     */
-    public AddDays createAddDays() {
-        return new AddDays();
     }
 
     /**
@@ -108,19 +84,19 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link RunFinancialEsc }
+     * Create an instance of {@link AddDays }
      * 
      */
-    public RunFinancialEsc createRunFinancialEsc() {
-        return new RunFinancialEsc();
+    public AddDays createAddDays() {
+        return new AddDays();
     }
 
     /**
-     * Create an instance of {@link IsMainframeOlieCheckFileRunningResponse }
+     * Create an instance of {@link AddDaysResponse }
      * 
      */
-    public IsMainframeOlieCheckFileRunningResponse createIsMainframeOlieCheckFileRunningResponse() {
-        return new IsMainframeOlieCheckFileRunningResponse();
+    public AddDaysResponse createAddDaysResponse() {
+        return new AddDaysResponse();
     }
 
     /**
@@ -132,19 +108,35 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link RunMainframeOlieCheckFile }
+     * Create an instance of {@link AddMonthsResponse }
      * 
      */
-    public RunMainframeOlieCheckFile createRunMainframeOlieCheckFile() {
-        return new RunMainframeOlieCheckFile();
+    public AddMonthsResponse createAddMonthsResponse() {
+        return new AddMonthsResponse();
     }
 
     /**
-     * Create an instance of {@link RunMainframeOlieCheckFileResponse }
+     * Create an instance of {@link GetCurrentTime }
      * 
      */
-    public RunMainframeOlieCheckFileResponse createRunMainframeOlieCheckFileResponse() {
-        return new RunMainframeOlieCheckFileResponse();
+    public GetCurrentTime createGetCurrentTime() {
+        return new GetCurrentTime();
+    }
+
+    /**
+     * Create an instance of {@link GetCurrentTimeResponse }
+     * 
+     */
+    public GetCurrentTimeResponse createGetCurrentTimeResponse() {
+        return new GetCurrentTimeResponse();
+    }
+
+    /**
+     * Create an instance of {@link RunFinancialEsc }
+     * 
+     */
+    public RunFinancialEsc createRunFinancialEsc() {
+        return new RunFinancialEsc();
     }
 
     /**
@@ -156,10 +148,58 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link AddMonthsResponse }
+     * Create an instance of {@link IsFinancialEscRunning }
      * 
      */
-    public AddMonthsResponse createAddMonthsResponse() {
-        return new AddMonthsResponse();
+    public IsFinancialEscRunning createIsFinancialEscRunning() {
+        return new IsFinancialEscRunning();
     }
+
+    /**
+     * Create an instance of {@link IsFinancialEscRunningResponse }
+     * 
+     */
+    public IsFinancialEscRunningResponse createIsFinancialEscRunningResponse() {
+        return new IsFinancialEscRunningResponse();
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Authentication }{@code >}
+     * 
+     * @param value
+     *     Java instance representing xml element's value.
+     * @return
+     *     the new instance of {@link JAXBElement }{@code <}{@link Authentication }{@code >}
+     */
+    @XmlElementDecl(namespace = "http://guidewire.com/ws/soapheaders", name = "authentication")
+    public JAXBElement<Authentication> createAuthentication(Authentication value) {
+        return new JAXBElement<Authentication>(_Authentication_QNAME, Authentication.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Locale }{@code >}
+     * 
+     * @param value
+     *     Java instance representing xml element's value.
+     * @return
+     *     the new instance of {@link JAXBElement }{@code <}{@link Locale }{@code >}
+     */
+    @XmlElementDecl(namespace = "http://guidewire.com/ws/soapheaders", name = "locale")
+    public JAXBElement<Locale> createLocale(Locale value) {
+        return new JAXBElement<Locale>(_Locale_QNAME, Locale.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link TransactionId }{@code >}
+     * 
+     * @param value
+     *     Java instance representing xml element's value.
+     * @return
+     *     the new instance of {@link JAXBElement }{@code <}{@link TransactionId }{@code >}
+     */
+    @XmlElementDecl(namespace = "http://guidewire.com/ws/soapheaders", name = "transaction_id")
+    public JAXBElement<TransactionId> createTransactionId(TransactionId value) {
+        return new JAXBElement<TransactionId>(_TransactionId_QNAME, TransactionId.class, null, value);
+    }
+
 }
