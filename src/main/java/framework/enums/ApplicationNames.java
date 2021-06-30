c12997eee46065d2822da23879a510c74d6fddae
Anurag Deshpande <adeshpande@idfbins.com>
2021-06-30 13:41:34 -0600

    adding full name for the application names

--- src/main/java/framework/enums/ApplicationNames.java
+++ src/main/java/framework/enums/ApplicationNames.java
@@ -1,10 +1,20 @@
 package framework.enums;
 
 public enum ApplicationNames {
-    CC,
-    BC,
-    PC,
-    AB,
-    ACCOUNT_MANAGEMENT_PORTAL,
-    FINANCE_CORE;
+    CC("ClaimCenter"),
+    BC("BillingCenter"),
+    PC("PolicyCenter"),
+    AB("ContactManager"),
+    ACCOUNT_MANAGEMENT_PORTAL("CustomerEngageAccountManagementPortal"),
+    FINANCE_CORE("FinanceCore");
+
+    private final String fullName;
+
+    ApplicationNames(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
 }
