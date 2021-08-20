2c05129d2f8c9d0c446b516f15cc31ecfb327b67
Anurag Deshpande <adeshpande@idfbins.com>
2021-08-20 11:55:08 -0600

    marking event update as a colon string for easier indexing

--- src/main/java/framework/logger/eventMessaging/IDefaultEventLoggingMessages.java
+++ src/main/java/framework/logger/eventMessaging/IDefaultEventLoggingMessages.java
@@ -2,19 +2,19 @@ package framework.logger.eventMessaging;
 
 public interface IDefaultEventLoggingMessages {
 
-    static <T extends LoggingEvent> String eventUpdateMessage(String testName, String className, T loggingEvent , String updateMessage){
-        String returnMessage = "Test="+testName+" Class="+className+" Event="+loggingEvent.getEventName()+" EventID="+loggingEvent.getEventID()+" EventUpdate="+updateMessage;
+    static <T extends LoggingEvent> String eventUpdateMessage(final String testName, final String className, final T loggingEvent, final String updateMessage) {
+        final String returnMessage = "Test=" + testName + " Class=" + className + " Event=" + loggingEvent.getEventName() + " EventID=" + loggingEvent.getEventID() + " EventUpdate: " + updateMessage;
         if (loggingEvent.getParentEventID().isPresent()) {
-            return returnMessage +" ParentEventID="+loggingEvent.getParentEventID().get();
+            return returnMessage + " ParentEventID=" + loggingEvent.getParentEventID().get();
         } else {
             return returnMessage;
         }
     }
 
-    static <T extends LoggingEvent> String eventEndMessage(String testName, String className, T loggingEvent , String timeTakenSeconds){
-        String returnMessage = "Test=" + testName + " Class="+className+" EventID=" + loggingEvent.getEventID() + " Event=" + loggingEvent.getEventName() + " Ended. TimeTaken=" + timeTakenSeconds;
-        if(loggingEvent.getParentEventID().isPresent()){
-            return returnMessage +" ParentEventID="+loggingEvent.getParentEventID().get();
+    static <T extends LoggingEvent> String eventEndMessage(final String testName, final String className, final T loggingEvent, final String timeTakenSeconds) {
+        final String returnMessage = "Test=" + testName + " Class=" + className + " EventID=" + loggingEvent.getEventID() + " Event=" + loggingEvent.getEventName() + " Ended. TimeTaken=" + timeTakenSeconds;
+        if (loggingEvent.getParentEventID().isPresent()) {
+            return returnMessage + " ParentEventID=" + loggingEvent.getParentEventID().get();
         } else {
             return returnMessage;
         }
