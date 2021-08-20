3430820834db50ba5895a99d5cf6854b56a122ba
Anurag Deshpande <adeshpande@idfbins.com>
2021-08-20 09:14:03 -0600

    adding if block to ensure throwable is not null before reading data off of it

--- src/main/java/framework/logger/regressionTestLogging/RegressionLogTemplates.java
+++ src/main/java/framework/logger/regressionTestLogging/RegressionLogTemplates.java
@@ -20,48 +20,50 @@ public class RegressionLogTemplates {
         System.out.println(ExceptionUtils.getThrowableList(new KnownDefectException("This is a test")).get(0).getClass().getSimpleName());
     }
 
-    public static String getLogTemplateForTestEndPass(ITestResult iTestResult, String... additionalTags){
+    public static String getLogTemplateForTestEndPass(ITestResult iTestResult, String... additionalTags) {
         return _getBaseTemplateTestEnd(iTestResult, Status.PASS.getName(), additionalTags);
     }
 
-    public static String getLogTemplateForTestEndFailed(ITestResult iTestResult, String... additionalTags){
+    public static String getLogTemplateForTestEndFailed(ITestResult iTestResult, String... additionalTags) {
         Throwable throwable = iTestResult.getThrowable();
-        String exceptionType = throwable.getClass().getSimpleName();
-        String exceptionMessage = throwable.getMessage();
-        List<String> strings = Arrays.asList(additionalTags);
-        strings.addAll(Arrays.asList("ExceptionType="+exceptionType, "ExceptionMessage="+exceptionMessage));
-        additionalTags = strings.toArray(new String[0]);
+        if (throwable != null) {
+            String exceptionType = throwable.getClass().getSimpleName();
+            String exceptionMessage = throwable.getMessage();
+            List<String> strings = Arrays.asList(additionalTags);
+            strings.addAll(Arrays.asList("ExceptionType=" + exceptionType, "ExceptionMessage=" + exceptionMessage));
+            additionalTags = strings.toArray(new String[0]);
+        }
         return _getBaseTemplateTestEnd(iTestResult, Status.FAIL.getName(), additionalTags);
     }
 
-    public static String getLogTemplateForTestEndFatal(ITestResult iTestResult, String... additionalTags){
+    public static String getLogTemplateForTestEndFatal(ITestResult iTestResult, String... additionalTags) {
         return _getBaseTemplateTestEnd(iTestResult, "FATAL", additionalTags);
     }
 
-    public static String getLogTemplateForTestEndSkipped(ITestResult iTestResult, String... additionalTags){
+    public static String getLogTemplateForTestEndSkipped(ITestResult iTestResult, String... additionalTags) {
         return _getBaseTemplateTestEnd(iTestResult, Status.SKIP.getName(), additionalTags);
     }
 
-    private static String _getBaseTemplateTestEnd(ITestResult iTestResult, String status, String... additionalTags){
+    private static String _getBaseTemplateTestEnd(ITestResult iTestResult, String status, String... additionalTags) {
         String logTemplate = _getBaseTestLifeCycleTemplate(buildTestDetailsDTO(iTestResult));
         String[] themes = iTestResult.getMethod().getConstructorOrMethod().getMethod().getAnnotationsByType(AutomatedTest.class)[0].Themes();
-        long timeTakenInSeconds = (new Timestamp(iTestResult.getEndMillis()).getTime() - new Timestamp(iTestResult.getStartMillis()).getTime())/1000;
-        if(ArrayUtils.isNotEmpty(themes)){
+        long timeTakenInSeconds = (new Timestamp(iTestResult.getEndMillis()).getTime() - new Timestamp(iTestResult.getStartMillis()).getTime()) / 1000;
+        if (ArrayUtils.isNotEmpty(themes)) {
             List<String> collect = Arrays.stream(themes).map(s -> s.replaceAll(" ", "_")).collect(Collectors.toList());
-            logTemplate = logTemplate+" Themes="+ Joiner.on(",").skipNulls().join(collect);
+            logTemplate = logTemplate + " Themes=" + Joiner.on(",").skipNulls().join(collect);
         }
 
-        logTemplate = logTemplate+" TimeTaken="+timeTakenInSeconds+" Seconds has Ended. TestStatus="+status;
+        logTemplate = logTemplate + " TimeTaken=" + timeTakenInSeconds + " Seconds has Ended. TestStatus=" + status;
 
-        if(ArrayUtils.isNotEmpty(additionalTags)){
+        if (ArrayUtils.isNotEmpty(additionalTags)) {
             logTemplate = logTemplate + Joiner.on(" ").skipNulls().join(additionalTags);
         }
 
         return logTemplate;
     }
 
-    private static String _getBaseTestLifeCycleTemplate(TestDetailsDTO testDetails){
-        return "TestName="+testDetails.getTestName()+" ClassName="+testDetails.getClassName()+" PackageName="+testDetails.getPackageName();
+    private static String _getBaseTestLifeCycleTemplate(TestDetailsDTO testDetails) {
+        return "TestName=" + testDetails.getTestName() + " ClassName=" + testDetails.getClassName() + " PackageName=" + testDetails.getPackageName();
     }
 
     public static TestDetailsDTO buildTestDetailsDTO(ITestResult iTestResult) {
